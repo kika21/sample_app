84b1e7cc26529af7ccfb0367849e686534d02ebc
keno <keno.kuhlmann@gmx.de>
2010-08-02 17:45:55 +0200

    Finished Layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 SampleApp::Application.routes.draw do |map|
-    match '/contact', :to => 'pages#contact'
-    match '/about',   :to => 'pages#about'
-    match '/help',    :to => 'pages#help'
+  get "users/new"
+
+  match '/signup',  :to => 'users#new'
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
 
   
 
